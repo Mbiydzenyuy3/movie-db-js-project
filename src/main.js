685462d6bfb100e579b9b8b02930181426fb5ca2
leila <mbiydzenyuyeileen@gmail.com>
2024-12-13 12:42:30 +0100

    added slider css style  and javascript functionality to the project

--- src/main.js
+++ src/main.js
@@ -3,216 +3,257 @@ import "./style.css";
 import "./carousel.js";
 import "./api.js";
 document.querySelector("#app").innerHTML = `
-  <header id="header">
-          <nav class="nav-bar">
-            <div class="logo">
-              <img src="./assets/img/Logo.png" alt="" class="logo-image" />
-              <span class="logo-name">
-                <img
-                  src="./assets/img/SaintStream.png"
-                  alt=""
-                  class="logo-name"
-                />
-              </span>
-            </div>
-            <div class="menu">
-              <a href="">Home</a>
-              <a href="">About</a>
-              <a href="">Favorite</a>
-            </div>
-            <div class = "icon-bar">
-              <form id="search-btn">
-                  <input type="text" class="input-field" />
-                  <img
-                    src="./assets/img/icons8-search-32.png"
-                    alt=""
-                    class="search-image"
-                  />
-                </form>
-              <div class="notify-icon">
-                <img
-                  src="./assets/img/icons8-notification-32.png"
-                  alt=""
-                  class="notify-image"
-                />
-              </div>
-              <div class="account-icon">
-                <img
-                  src="./assets/img/Image(1).png"
-                  alt=""
-                  class="account-image"
-                />
-                <div class="arrow-down">
-                  <img
-                    src="./assets/img/Arrow - Down 2.png"
-                    alt=""
-                    class="arrow-image"
-                  />
-                </div>
-              </div>
-            </div>
-          </nav>
-          <div class="search-bar">
-            <form for ="" method = "GET" id="form">
-              <label for="get" class="search-icon">
-                <input type="text" id="input-field" />
-                <button type ="button" class= "search-btn">
-                  <img src="./assets/img/icons8-search-32.png" alt="" class="search-image"/>
-                </button>
-              </label>
-            </form>
-          </div>
-  </header>
-  <section id="hero-section" style = "background-image: url()">
-    <div class = "carousel-images">
-      <div class = "carousel" >
+<header>
+      <div class="logo">
+        <div class="logo-image">
+          <img class="main-logo" src="./assets/img/Logo.png" alt="logo" style="width: 40px" />
+        </div>
+        <div class="logo-name">
+          <img class="saintstream" src="./assets/img/SaintStream.png" alt="">
+        </div>
       </div>
-      <div class="overlay-position">
-          <p class="paragraph-one">
-            Watch the best trailers and add to your favourite and watchlist.    
-          </p>
-          <span class="rating"></span>
-          <div class = "buttons">
-            <button id="cta" type="button">
-              <span>Watch Trailer</span>
+      <div class="navbar">
+        <a href="./home.html">Home</a>
+        <a href="./movie.html">Movies</a>
+        <a href="./favorites.html">tv Shows</a>
+      </div>
+      <div class="searchbar">
+        <form>
+          <div class="searchboxbar">
+            <div class="searchbox">
+              <input type="text" placeholder="Search ..." />
+            </div>
+            <div class="search">
+              <button class="Search-icon material-symbols-outlined" id="iconsea">search
               </button>
-              <button id="counter" type="button">Add watchlist</button>
+            </div>
           </div>
+        </form>
+      </div>
+    </header>
+
+    <section class="hero-section swiper">
+      <div class="swiper-wrapper slides ">
+        <div class=" swiper-slide slide">
+          <img class="image-item first-image" src="./assets/img/heroImage.webp" alt="image" />
+          <div class="text">
+            <h1 class="movie-title"></h1>
+            <p class="movie-overwiew">
+            </p>
+          </div>
+        </div>
+        <div class="swiper-slide slide">
+          <img class="image-item first-image" src="./assets/img/furry.jpg" alt="image" />
+          <div class="text">
+            <h1 class="movie-title"></h1>
+            <p class="movie-overwiew">
+            </p>
+          </div>
+        </div>
+        <div class="swiper-slide slide">
+          <img class="image-item first-image" src="./assets/img/damsel.jpg" alt="image" />
+          <div class="text">
+            <h1 class="movie-title"></h1>
+            <p class="movie-overwiew">
+            </p>
+          </div>
+        </div>
+        <div class="swiper-slide slide">
+          <img class="image-item first-image" src="./assets/img/lucifer.jpg" alt="image" />
+          <div class="text">
+            <h1 class="movie-title"></h1>
+            <p class="movie-overwiew">
+            </p>
+          </div>
+        </div>
       </div>
       <div style="text-align: right; width: 100%">
-        <span class="dots"></span> 
-        <span class="dots"></span> 
-        <span class="dots"></span> 
+        <span class="dots"></span>
+        <span class="dots"></span>
+        <span class="dots"></span>
       </div>
-    </div>
-  </section>
-  <section class="week-popular">
-    <div class="pop"><h3>Most Recent Movies</h3></div>
-    <div class="second-catt">
-      <div class="cattii-i">
-        <div class="num">1</div>
-        <div class="cattii-img">
-          <img src="./images/imagee.avif" alt="img" />
+    </section>
+    <section class="brand-logos">
+      <div class="logo-slider">
+        <div class="logo-one">
+          <img src="/assets/img/disney.png" alt="" class="logos" />
         </div>
-        <div class="cattii-t">
-          <div class="pg">PG-13</div>
-          <h4>wonder woman</h4>
-          <span class="supp"><p>SuperHero.Action</p></span>
-          <p>⭐ 4.3| Movies</p>
+        <div class="logo-two"><img src="/assets/img/netflix.png" alt="" class="logos" />
         </div>
+        <div class="logo-three"><img src="/assets/img/hbo-max.png" alt="" class="logos" /></div>
+        <div class="logo-four"><img src="/assets/img/pixar.png" alt="" class="logos" /></div>
+        <div class="logo-five"><img src="/assets/img/marvel.png" alt="" class="logos" /></div>
+        <div class="logo-six"><img src="/assets/img/starwars.png" alt="" class="logos" /></div>
+        <div class="logo-seven"><img src="/assets/img/national geographic.png" alt="" class="logos" /></div>
+        <div class="logo-seven"><img src="/assets/img/youTube.png" alt="" class="logos" /></div>
+        <div class="logo-seven"><img src="/assets/img/webseries.png" alt="" class="logos" /></div>
       </div>
-      <div class="cattii-i">
-        <div class="num">2</div>
-        <div class="cattii-img">
-          <img src="./images/imagee.avif" alt="img" />
-        </div>
-        <div class="cattii-t">
-          <div class="pg">PG-13</div>
-          <h4>wonder woman</h4>
-          <span class="supp"><p>SuperHero.Action</p></span>
-          <p>⭐ 4.3| Movies</p>
-        </div>
+    </section>
+    <section class=" swiper popular-release">
+      <div class="heading-three-item">
+        <h3 class="movie-heading-popular">Popular of the week</h3>
       </div>
-      <div class="cattii-i">
-        <div class="num">3</div>
-        <div class="cattii-img">
-          <img src="./images/imagee.avif" alt="img" />
+      <div class="new-popular-wrapper swiper-wrapper">
+        <div class="new-popular-item swiper-slide">
+          <div class="number">1</div>
+          <div class="image-popular-release">
+            <img class="poster-img" src="./assets/img/badboys.jpg" alt="img">
+          </div>
+          <div class="release-overwiew">
+            <div class="pg-age">PG-13</div>
+            <h4>wonder woman</h4>
+            <span class="movie-genre">
+              <p>SuperHero.Action</p>
+            </span>
+            <p class="movie-star">⭐ 4.3| Movies</p>
+          </div>
         </div>
-        <div class="cattii-t">
-          <div class="pg">PG-13</div>
-          <h4>wonder woman</h4>
-          <span class="supp"><p>SuperHero.Action</p></span>
-          <p>⭐ 4.3| Movies</p>
+        <div class="new-popular-item swiper-slide">
+          <div class="number">2</div>
+          <div class="image-popular-release">
+            <img class="poster-img" src="./assets/img/thebeekeeper.jpg" alt="img">
+          </div>
+          <div class="release-overwiew">
+            <div class="pg-age">PG-13</div>
+            <h4>wonder woman</h4>
+            <span class="movie-genre">
+              <p>SuperHero.Action</p>
+            </span>
+            <p class="movie-star">⭐ 4.3| Movies</p>
+          </div>
         </div>
-      </div>
-      <div class="cattii-i">
-        <div class="num">4</div>
-        <div class="cattii-img">
-          <img src="./images/imagee.avif" alt="img" />
+        <div class="new-popular-item swiper-slide">
+          <div class="number">3</div>
+          <div class="image-popular-release">
+            <img class="poster-img" src="./assets/img/lioness(1).jpg" alt="img">
+          </div>
+          <div class="release-overwiew">
+            <div class="pg-age">PG-13</div>
+            <h4>wonder woman</h4>
+            <span class="movie-genre">
+              <p>SuperHero.Action</p>
+            </span>
+            <p class="movie-star">⭐ 4.3| Movies</p>
+          </div>
+        </div>        <div class="new-popular-item swiper-slide">
+          <div class="number">3</div>
+          <div class="image-popular-release">
+            <img class="poster-img" src="./assets/img/lioness(1).jpg" alt="img">
+          </div>
+          <div class="release-overwiew">
+            <div class="pg-age">PG-13</div>
+            <h4>wonder woman</h4>
+            <span class="movie-genre">
+              <p>SuperHero.Action</p>
+            </span>
+            <p class="movie-star">⭐ 4.3| Movies</p>
+          </div>
+        </div>        <div class="new-popular-item swiper-slide">
+          <div class="number">3</div>
+          <div class="image-popular-release">
+            <img class="poster-img" src="./assets/img/lioness(1).jpg" alt="img">
+          </div>
+          <div class="release-overwiew">
+            <div class="pg-age">PG-13</div>
+            <h4>wonder woman</h4>
+            <span class="movie-genre">
+              <p>SuperHero.Action</p>
+            </span>
+            <p class="movie-star">⭐ 4.3| Movies</p>
+          </div>
         </div>
-        <div class="cattii-t">
-          <div class="pg">PG-13</div>
-          <h4>wonder woman</h4>
-          <span class="supp"><p>SuperHero.Action</p></span>
-          <p>⭐ 4.3| Movies</p>
-        </div>
-      </div>
-  </section>
-  <section class="brand-logos">
-    <div class="logo-slider">
-      <div class ="logo-one">
-      <img src="/assets/img/disney.png" alt="" class="logos" />
-      </div>
-      <div class ="logo-two"><img src="/assets/img/netflix.png" alt="" class="logos" /></div>
-      <div class ="logo-three"><img src="/assets/img/hbo-max.png" alt="" class="logos" /></div>
-      <div class ="logo-four"><img src="/assets/img/pixar.png" alt="" class="logos" /></div>
-      <div class ="logo-five"><img src="/assets/img/marvel.png" alt="" class="logos" /></div>
-      <div class ="logo-six"><img src="/assets/img/starwars.png" alt="" class="logos" /></div>
-      <div class ="logo-seven"><img src="/assets/img/national geographic.png" alt="" class="logos" /></div>
-      <div class ="logo-seven"><img src="/assets/img/youTube.png" alt="" class="logos" /></div>
-      <div class ="logo-seven"><img src="/assets/img/webseries.png" alt="" class="logos" /></div>
-    </div>
-  </section>
-  <section id = "movies">
-    <div class = "content">
-    <h2>Popular Movies</h2>
-    <div class="watch-list" >
-      <img src = "" alt = "image-jpg">
-      <div id="overlay-one">
-        <div class ="watch-content">
-          <h3></h3>
-          <p class="read-the-docs">
-          </p>
-        </div>
-        <button id="counter" type="button">add to list</button>
-      </div>
-    </div> 
-    </div>
-    <div class = "content">
-      <h2>Trending Movies</h2>
-      <div id = "hero" class ="carousel-wrapper">
-        <div id = "poster-images" class = "carousel-slides favorites" >
-          <div class="poster-details"></div>
-        </div>
-      </div>
-    </div>
-    <div class = "content">
-      <h2>Upcoming Movies</h2>
-      <div id="liked-movies">
-      <img src = "" alt = "image-jpg">
-      <div id="overlay-three">
-        <div class ="liked-content">
-          <h3></h3>
-          <p class="read-the-docs">
-          </p>
-        </div>
-        <button id="counter" type="button"></button>
-      </div>
-    </div>
-    </div>
-  </section>
-  <footer>
-    <div class = "footer">
-      <div class = "footer-item-one">
-        <div class="footer-heading">
-          <h3>Our platform is trusted by millions & features best updated movies all around the world.</h3>
+        <div class="new-popular-item swiper-slide">
+          <div class="number">4</div>
+          <div class="image-popular-release">
+            <img class="poster-img" src="./assets/img/kraven.jpg" alt="img">
+          </div>
+          <a href="" class="release-overwiew">
+            <div class="pg-age">PG-13</div>
+            <h4>wonder woman</h4>
+            <span class="movie-genre">
+              <p>SuperHero.Action</p>
+            </span>
+            <p class="movie-star">⭐ 4.3| Movies</p>
+          </a>
         </div>
       </div>
-      <div class = "footer-item-two">
-        <ul class="unordered-list">
-          <div class="links"><a href="">Home</a>/</div>
-          <div class="links"><a href="">Release</a>/</div>
-          <div class="links"><a href="">Discover</a>/</div>
-        </ul>
-        <div class="social-media">
-          <img class = "social-media-icon" src="/assets/img/Facebook.png" alt="facebook-icon" />
-          <img class = "social-media-icon" src="/assets/img/Instagram.png" alt="instagram-icon" />
-          <img class = "social-media-icon" src="/assets/img/Twitter.png" alt="twitter-icon" />
-          <img class = "social-media-icon" src="/assets/img/Google.png" alt="google-icon" />
+      <div class="swiper-button-next"></div>
+
+    </section>
+    <section id="new-release">
+      <div class="new-release">
+        <h3>Just Realesed</h3>
+      </div>
+      <div class="item-wrapper">
+        <div class="img-release-wrap">
+          <img class="movie-release-poster" src="./assets/img/kingdomofapes.jpg" alt="" />
+          <div class="movie-release-overview">
+            <h3>wonder Woman</h3>
+            <p class="movie-release-paragraph"><span class="rate-star">⭐4.6</span>| SuperHero.movie</p>
+          </div>
+        </div>
+        <div class="img-release-wrap">
+          <img class="movie-release-poster" src="./assets/img/kingdomofapes.jpg" alt="" />
+          <div class="movie-release-overview">
+            <h3>wonder Woman</h3>
+            <p class="movie-release-paragraph"><span class="rate-star">⭐4.6</span>| SuperHero.movie</p>
+          </div>
+        </div>
+        <div class="img-release-wrap">
+          <img class="movie-release-poster" src="./assets/img/kingdomofapes.jpg" alt="" />
+          <div class="movie-release-overview">
+            <h3>wonder Woman</h3>
+            <p class="movie-release-paragraph"><span class="rate-star">⭐4.6</span>| SuperHero.movie</p>
+          </div>
+        </div>
+        <div class="img-release-wrap">
+          <img class="movie-release-poster" src="./assets/img/kingdomofapes.jpg" alt="" />
+          <div class="movie-release-overview">
+            <h3>wonder Woman</h3>
+            <p class="movie-release-paragraph"><span class="rate-star">⭐4.6</span>| SuperHero.movie</p>
+          </div>
+        </div>
+        <div class="img-release-wrap">
+          <img class="movie-release-poster" src="./assets/img/kingdomofapes.jpg" alt="" />
+          <div class="movie-release-overview">
+            <h3>wonder Woman</h3>
+            <p class="movie-release-paragraph"><span class="rate-star">⭐4.6</span>| SuperHero.movie</p>
+          </div>
+        </div>
+        <div class="img-release-wrap">
+          <img class="movie-release-poster" src="./assets/img/kingdomofapes.jpg" alt="" />
+          <div class="movie-release-overview">
+            <h3>wonder Woman</h3>
+            <p class="movie-release-paragraph"><span class="rate-star">⭐4.6</span>| SuperHero.movie</p>
+          </div>
+        </div>
+
+      </div>
+
+    </section>
+    <footer>
+      <div class="footer">
+        <div class="footer-item-one">
+          <div class="footer-heading">
+            <h3>Our platform is trusted by millions & features best updated movies all around the world.
+            </h3>
+          </div>
+        </div>
+        <div class="footer-item-two">
+          <ul class="unordered-list">
+            <div class="links"><a href="">Home</a>/</div>
+            <div class="links"><a href="">Release</a>/</div>
+            <div class="links"><a href="">Discover</a>/</div>
+          </ul>
+          <div class="social-media">
+            <img class="social-media-icon" src="/assets/img/Facebook.png" alt="facebook-icon" />
+            <img class="social-media-icon" src="/assets/img/Instagram.png" alt="instagram-icon" />
+            <img class="social-media-icon" src="/assets/img/Twitter.png" alt="twitter-icon" />
+            <img class="social-media-icon" src="/assets/img/Google.png" alt="google-icon" />
+          </div>
         </div>
       </div>
-      </div>
-      <div class = "contact-footer">
+      <div class="contact-footer">
         <div class="contact-policy">
           <span>Privacy Policy</span>
           <span>Terms of service</span>
@@ -222,8 +263,34 @@ document.querySelector("#app").innerHTML = `
           &copy 2024
         </div>
       </div>
-  </footer>
+    </footer>
 `;
+
+/*swiper js config */
+var swiper = new Swiper(".hero-section", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper(".popular-release", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+      nextEl: ".swiper-button-next",
+    },
+});
 
 const options = {
   method: "GET",

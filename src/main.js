fa93c925c5eaaa06852f50bda2de75844a5ea7b3
leila <mbiydzenyuyeileen@gmail.com>
2024-12-17 08:47:01 +0100

    commiting new changes to the project the details page

--- src/main.js
+++ src/main.js
@@ -28,36 +28,13 @@ document.querySelector("#app").innerHTML = `
 </header>
 
  <section class="hero-banner hero-swiper">
+          <div class="bg-image" id="slider"></div>
             <div class="swiper-slide slide  slide-banner overlay">
                 <div class="container swiper-container">
-                    <div class="movie-info">
-                        <span class="movie-tag">
-                            season 6
-                        </span>
-                        <h3 class="movie-title">
-                            Star Wars: Mandalorian
-                        </h3>
-                        <span class="movie-meta">
-                            2hr40m - 2022 - Fantasy - Action
-                        </span>
-                        <p class="movie-discription">
-                            The Mandalorian protagonist, Din Djarin,but usually called “The Mandalorian” or shortened
-                            “Mando”, is a lone fighter who earns his money as a bounty hunter five years after the sheer
-                            fall of the Empire.
-                        </p>
-                        <div class="cta">
-                            <button class="primary-cta">
-                              <img src="/assets/img/play.png" alt="button play icon">
-                              <span>Play now</span>  
-                            </button>
-                            <button class="secondary-cta">
-                              <span> Watch trailer</span>
-                            </button>
-                            <button class="outline-cta">
-                             <img src="/assets/img/bookmark.png" alt="button play icon">
-                             <span>Add watchlist</span>
-                            </button>
-                        </div>
+                    <div class="movie-info" id="hero-content">
+                       <h1 class="main-heading"></h1>
+                       <p heading-paragraph></p>
+                        
                     </div>
                 </div>
             </div>
@@ -78,31 +55,32 @@ document.querySelector("#app").innerHTML = `
         <div class="logo-item"><img src="/assets/img/webseries.png" alt="" class="logos" /></div>
       </div>
     </section>
-    <section class="popular-release">
+    <section class="popular-release mySwiper" id="popular-release">
     <div class="container swiper">
-        <div class="heading-three-item">
+        <div class="heading-three-item" swiper-slide>
           <h3 class="movie-heading-popular">Popular of the week</h3>
         </div>
-        <div class="new-popular-wrapper swiper-wrapper">
-          <div class="new-popular-item swiper-slide">
+        <div class="new-popular-wrapper swiper-slide">
+        
+          <div class="new-popular-item">
             <div class="number">1</div>
-            <a href="" class="image-popular-release">
+            <div class="image-popular-release">
               <img class="poster-img" src="/assets/img/badboys.jpg" alt="img">
-            </a>
+            </div>
             <div class="release-overwiew">
               <div class="pg-age">PG-13</div>
-              <h4 class="new-release-title">wonder woman</h4>
+              <h4 class="new-release-title">Bad Boys</h4>
               <span class="movie-genre">
                 <p>SuperHero - Action</p>
               </span>
               <p class="movie-star">⭐ 4.3 | <span>Movies</span></p>
             </div>
           </div>
-          <div class="new-popular-item swiper-slide">
+          <div class="new-popular-item">
             <div class="number">2</div>
-            <a href="" class="image-popular-release">
+            <div class="image-popular-release">
               <img class="poster-img" src="/assets/img/furry.jpg" alt="img">
-            </a>
+            </div>
             <div class="release-overwiew">
               <div class="pg-age">PG-13</div>
               <h4 class="new-release-title">Mad Furry</h4>
@@ -112,11 +90,11 @@ document.querySelector("#app").innerHTML = `
               <p class="movie-star">⭐ 4.3 | <span>Movies</span></p>
             </div>
          </div>
-         <div class="new-popular-item swiper-slide">
+         <div class="new-popular-item">
             <div class="number">3</div>
-            <a href="" class="image-popular-release">
+            <div class="image-popular-release">
               <img class="poster-img" src="/assets/img/damsel.jpg" alt="img">
-            </a>
+            </div>
             <div class="release-overwiew">
               <div class="pg-age">PG-13</div>
               <h4 class="new-release-title">Damsel</h4>
@@ -126,11 +104,11 @@ document.querySelector("#app").innerHTML = `
               <p class="movie-star">⭐ 4.3 | <span>Movies</span></p>
             </div>
           </div>
-          <div class="new-popular-item swiper-slide">
+          <div class="new-popular-item">
             <div class="number">4</div>
-            <a href="" class="image-popular-release">
+            <div class="image-popular-release">
               <img class="poster-img" src="/assets/img/hellboy.jpg" alt="img">
-            </a>
+            </div>
             <div class="release-overwiew">
               <div class="pg-age">PG-13</div>
               <h4 class="new-release-title">Hellboy</h4>
@@ -140,11 +118,11 @@ document.querySelector("#app").innerHTML = `
               <p class="movie-star">⭐ 4.3 | <span>Movies</span></p>
             </div>
           </div>
-          <div class="new-popular-item swiper-slide">
+          <div class="new-popular-item">
             <div class="number">5</div>
-            <a href="" class="image-popular-release">
+            <div class="image-popular-release">
               <img class="poster-img" src="/assets/img/gardians(1).jpg" alt="img">
-            </a>
+            </div>
             <div class="release-overwiew">
               <div class="pg-age">PG-13</div>
               <h4 class="new-release-title">Gardians of the Galaxy</h4>
@@ -154,11 +132,11 @@ document.querySelector("#app").innerHTML = `
               <p class="movie-star">⭐ 4.3 | <span>Movies</span></p>
             </div>
           </div>
-          <div class="new-popular-item swiper-slide">
+          <div class="new-popular-item ">
             <div class="number">6</div>
-            <a href="" class="image-popular-release">
+            <div class="image-popular-release">
               <img class="poster-img" src="/assets/img/kingdomofapes.jpg" alt="img">
-            </a>
+            </div>
             <div class="release-overwiew">
               <div class="pg-age">PG-13</div>
               <h4 class="new-release-title">Kingdom of Apes</h4>
@@ -168,11 +146,11 @@ document.querySelector("#app").innerHTML = `
               <p class="movie-star">⭐ 4.3 | <span>Movies</span></p>
             </div>
           </div>
-          <div class="new-popular-item swiper-slide">
+          <div class="new-popular-item ">
             <div class="number">7</div>
-            <a href="" class="image-popular-release">
+            <div class="image-popular-release">
               <img class="poster-img" src="/assets/img/thebeekeeper.jpg" alt="img">
-            </a>
+            </div>
             <div class="release-overwiew">
               <div class="pg-age">PG-13</div>
               <h4 class="new-release-title">The Beekeeper</h4>
@@ -181,11 +159,11 @@ document.querySelector("#app").innerHTML = `
               </span>
               <p class="movie-star">⭐ 4.3 | <span>Movies</span></p>
             </div>
-          </div><div class="new-popular-item swiper-slide">
+          </div><div class="new-popular-item ">
             <div class="number">8</div>
-            <a href="" class="image-popular-release">
+            <div class="image-popular-release">
               <img class="poster-img" src="/assets/img/kraven.jpg" alt="img">
-            </a>
+            </div>
             <div class="release-overwiew">
               <div class="pg-age">PG-13</div>
               <h4 class="new-release-title">Kraven</h4>
@@ -195,11 +173,11 @@ document.querySelector("#app").innerHTML = `
               <p class="movie-star">⭐ 4.3 | <span>Movies</span></p>
             </div>
           </div>
-          <div class="new-popular-item swiper-slide">
+          <div class="new-popular-item ">
             <div class="number">9</div>
-            <a href="" class="image-popular-release">
+            <div class="image-popular-release">
               <img class="poster-img" src="/assets/img/lioness(1).jpg" alt="img">
-            </a>
+            </div>
             <div class="release-overwiew">
               <div class="pg-age">PG-13</div>
               <h4 class="new-release-title">Lioness</h4>
@@ -209,11 +187,11 @@ document.querySelector("#app").innerHTML = `
               <p class="movie-star">⭐ 4.3 | <span>Movies</span></p>
             </div>
           </div>
-          <div class="new-popular-item swiper-slide">
+          <div class="new-popular-item ">
             <div class="number">10</div>
-            <a href="" class="image-popular-release">
+            <div class="image-popular-release">
               <img class="poster-img" src="/assets/img/lucifer.jpg" alt="img">
-            </a>
+            </div>
             <div class="release-overwiew">
               <div class="pg-age">PG-13</div>
               <h4 class="new-release-title">Lucifer</h4>
@@ -223,11 +201,11 @@ document.querySelector("#app").innerHTML = `
               <p class="movie-star">⭐ 4.3 | <span>Movies</span></p>
             </div>
           </div>
-        <div class="new-popular-item swiper-slide">
+        <div class="new-popular-item ">
             <div class="number">11</div>
-            <a href="" class="image-popular-release">
+            <div class="image-popular-release">
               <img class="poster-img" src="/assets/img/imagee.avif" alt="img">
-            </a>
+            </div>
             <div class="release-overwiew">
               <div class="pg-age">PG-13</div>
               <h4 class="new-release-title">Avif</h4>
@@ -237,11 +215,11 @@ document.querySelector("#app").innerHTML = `
               <p class="movie-star">⭐ 4.3 | <span>Movies</span></p>
             </div>
           </div>
-          <div class="new-popular-item swiper-slide">
+          <div class="new-popular-item ">
             <div class="number">12</div>
-            <a href="" class="image-popular-release">
+            <div class="image-popular-release">
               <img class="poster-img" src="/assets/img/after.jpg" alt="img">
-            </a>
+            </div>
             <div class="release-overwiew">
               <div class="pg-age">PG-13</div>
               <h4 class="new-release-title">After</h4>
@@ -251,11 +229,11 @@ document.querySelector("#app").innerHTML = `
               <p class="movie-star">⭐ 4.3 | <span>Movies</span></p>
             </div>
           </div>
-          <div class="new-popular-item swiper-slide">
+          <div class="new-popular-item ">
             <div class="number">13</div>
-            <a href="" class="image-popular-release">
+            <div class="image-popular-release">
               <img class="poster-img" src="/assets/img/fifthy shades.jpg" alt="img">
-            </a>
+            </div>
             <div class="release-overwiew">
               <div class="pg-age">PG-13</div>
               <h4 class="new-release-title">Fifthy Shades</h4>
@@ -265,11 +243,11 @@ document.querySelector("#app").innerHTML = `
               <p class="movie-star">⭐ 4.3 | <span>Movies</span></p>
             </div>
           </div>
-          <div class="new-popular-item swiper-slide">
+          <div class="new-popular-item >
             <div class="number">14</div>
-            <a href="" class="image-popular-release">
+            <div class="image-popular-release">
               <img class="poster-img" src="/assets/img/first-match(1).jpg" alt="img">
-            </a>
+            </div>
             <div class="release-overwiew">
               <div class="pg-age">PG-13</div>
               <h4 class="new-release-title">First Match</h4>
@@ -282,35 +260,41 @@ document.querySelector("#app").innerHTML = `
        </div>
 </section>
 
-<section id="just-release">
-  <div class="container">
-    <div class="release-heading-wrapper">
+<section id="just-release" class="mySwiper swiper">
+  <div class="container swiper-wrapper">
+    <div class="release-heading-wrapper swiper-slide">
       <h3 class="heading">Just Release</h3>
     </div>
-    <div class="just-release-wrapper">
+    <div class="just-release-wrapper swiper-slide">
             
     </div>
+    <div class="swiper-button-next"></div>
+    <div class="swiper-button-prev"></div>
   </div>
 </section>
 
-<section id="watch-list">
-  <div class = "container">
-    <div class="watch-list-heading">
-        <h3 class="heading">Your Watchlist</h3>
+<section id="watch-list" class="mySwiper swiper">
+  <div class = "container swiper-wrapper">
+    <div class="watch-list-heading swiper-slide">
+        <h3 class="heading">Your Watchlist </h3>
     </div>
-      <div class="item-wrapper">
+    <div class="item-wrapper swiper-slide">
         
     </div>
+    <div class="swiper-button-next"></div>
+    <div class="swiper-button-prev"></div>
   </div>
 </section>
-<section id="likes">
-    <div class = "container">
+<section id="likes" class="mySwiper swiper">
+    <div class = "container swiper-wrapper">
         <div class="likes-heading">
           <h3 class="heading">Likes</h3>
         </div>
-      <div class="item-wrapper-one">
+      <div class="item-wrapper-one swiper-slide">
         
       </div>
+      <div id="swiper-button-next"></div>
+     <div id="swiper-button-prev"></div>
       </div>
     </section>
     <footer>
@@ -350,32 +334,6 @@ document.querySelector("#app").innerHTML = `
     </footer>
 `;
 
-/*swiper js config */
-let swiper = new Swiper(".hero-section", {
-  spaceBetween: 30,
-  centeredSlides: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
-
-swiper = new Swiper(".swiper", {
-  slidesPerView: 4,
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-  },
-});
-
 // setupCounter(document.querySelector("#counter"));
 
 // API for release movie section
@@ -400,18 +358,113 @@ fetch(
       const posterPath = movie.poster_path
         ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
         : "https://via.placeholder.com/500x750?text=No+Image+Available";
-
+      const addDiv = document.createElement("div");
+      addDiv.className = "released-movies";
+      firstSection.appendChild(addDiv);
       const img = document.createElement("img");
       img.src = posterPath;
       img.alt = movie.title;
       img.className = "movie-poster-one";
-      firstSection.appendChild(img);
-
-      // movieParagraph.className = "movie-paragraph";
-      // movieParagraph.textContent = movie.paragraph;
-      // movieParagraph.innerHTML = `&#11088; ${movie.vote_average} | Mystery - Movies`;
-      // addDiv.appendChild(movieTitle);
-      // addDiv.appendChild(movieParagraph);
+      addDiv.appendChild(img);
+      const movieParagraph = document.createElement("p");
+      movieParagraph.className = "movie-paragraph";
+      movieParagraph.textContent = movie.paragraph;
+      movieParagraph.innerHTML = `&#11088; ${movie.vote_average} | Mystery - Movies`;
+      addDiv.appendChild(movieParagraph);
     }
   })
   .catch((err) => console.error(err));
+
+const API_KEY = "688d9e5aaae7b1a265762fb4097fd8b6";
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
+
+// DOM Elements
+const heroSlider = document.getElementById("slider");
+const heroContent = document.getElementById("hero-content");
+const popularMovies = document.getElementById("popular-release");
+
+// Global variables
+let currentIndex = 0;
+let movies = [];
+let genresMap = {};
+
+function startSlider() {
+  setInterval(() => {
+    currentIndex = (currentIndex + 1) % movies.length;
+    slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+    updateHeroText(currentIndex);
+  }, 5000);
+}
+fetchTrendingMovies();
+let currentSlide = 0;
+let trendingMovies = [];
+// Fetch trending movies for the hero slider
+async function fetchTrendingMovies() {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`
+  );
+  const data = await response.json();
+  trendingMovies = data.results.slice(0, 4);
+  renderHeroSlider(trendingMovies);
+  startHeroSlider();
+}
+// Fetch generic movie data
+async function fetchMovies(endpoint, renderFunction, limit = 10) {
+  const response = await fetch(endpoint);
+  const data = await response.json();
+  renderFunction(data.results.slice(0, limit));
+}
+// Render Hero Slider
+function renderHeroSlider(movies) {
+  heroSlider.innerHTML = movies
+    .map(
+      (movie) => `
+        <img src="${IMAGE_BASE_URL}${movie.backdrop_path}" alt="${movie.title}">
+    `
+    )
+    .join("");
+  updateHeroContent(0);
+}
+// Render Section Sliders
+function renderMovieSlider(movies, container) {
+  container.innerHTML = movies
+    .map(
+      (movie) => `
+        <img src="${IMAGE_BASE_URL}${movie.poster_path}" alt="${movie.title}">
+    `
+    )
+    .join("");
+}
+// Update Hero Content
+function updateHeroContent(index) {
+  const movie = trendingMovies[index];
+  heroContent.innerHTML = `
+        <h1>${movie.title}</h1>
+        <p>${movie.overview}</p>
+        <div class="hero-buttons">
+          <div class="cta">
+            <button class="primary-cta">
+              <img src="/assets/img/play.png" alt="button play icon">
+              <span>Play now</span>  
+            </button>
+            <button class="secondary-cta">
+              <span> Watch trailer</span>
+            </button>
+            <a href="watch-list">
+              <button class="outline-cta">
+                <img src="/assets/img/bookmark.png" alt="button play icon">
+                <span>Add watchlist</span>
+              </button>
+            </a>
+        </div>
+        </div>
+    `;
+}
+// Start Auto Slide for Hero
+function startHeroSlider() {
+  setInterval(() => {
+    currentSlide = (currentSlide + 1) % trendingMovies.length;
+    heroSlider.style.transform = `translateX(-${currentSlide * 100}%)`;
+    updateHeroContent(currentSlide);
+  }, 10000);
+}
